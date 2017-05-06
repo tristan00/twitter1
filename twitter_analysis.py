457deea76613cfbe3db12f4c61854e557870a7de
TD\tdelforge <tristandelforge1@gmail.com>
2017-05-06 13:42:19 -0700

    db added

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,10 +1,12 @@
 from TwitterSearch import *
 import sqlite3
+import traceback
 
 access_token = None
 access_token_secret = None
 consumer_key = None
 consumer_secret = None
+ts = None # twitter connection object
 
 def get_auth():
     global access_token
@@ -19,28 +21,65 @@ def get_auth():
     for i in fields:
         #fields_dict[i[0]] = i[1]
         if i[0] == 'API Key':
-            consumer_key = i[1] 
+            consumer_key = i[1]
         if i[0] == 'API Secret':
             consumer_secret = i[1]
         if i[0] == 'Access Token':
             access_token = i[1]
         if i[0] == 'Access Token Secret':
             access_token_secret = i[1]
+    conn.close()
 
-def main():
-    get_auth()
-    #print(consumer_key, consumer_secret, access_token, access_token_secret)
-    tuo = TwitterUserOrder('BarackObama')
+def read_users_tweets(user):
+    tuo = TwitterUserOrder(user)
 
-    ts = TwitterSearch(consumer_key,
-                    consumer_secret,
-                    access_token,
-                    access_token_secret)
+    #set up db connection
+    conn = sqlite3.connect('twitter.db')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('''CREATE TABLE twitter_data(user text, tweet text, reply_to text, PRIMARY KEY(user, tweet))''')
+    except:
+        #db already created
+        pass
+
 
     for tweet in ts.search_tweets_iterable(tuo):
-        print(tweet)
-        return
+        user = tweet['user']['screen_name']
+        tweet_text = tweet['text']
+        try:
+            reply_to = tweet['quoted_status']['user']['screen_name']
+        except:
+            reply_to = None
+
+        try:
+            cursor.execute('INSERT INTO twitter_data VALUES (?,?,?)', (user, tweet_text,reply_to))
+            conn.commit()
+        except:
+            pass
+            #traceback.print_exc()
+
+    conn.close()
 
 
+def get_tweet_count():
+    conn = sqlite3.connect('twitter.db')
+    cursor = conn.cursor()
+    fields = cursor.execute('select * from twitter_data')
+    print(len(fields.fetchall()))
+
+def main():
+    global ts
+
+    get_auth()
+    ts = TwitterSearch(consumer_key,consumer_secret,access_token,access_token_secret)
+
+    users = ['BarackObama']
+    for u in users:
+        try:
+            read_users_tweets(u)
+        except:
+            pass
+    get_tweet_count()
 
 main()
+
